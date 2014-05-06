ee06d11d6cda46ce21948b82ad1a04acf17544ce
Darius Roberts <darius.roberts@gmail.com>
2014-05-06 06:19:41 -0400

    adding label for text and password fields, correcting href and method sigs

--- js/recordGherkin.js
+++ js/recordGherkin.js
@@ -20,26 +20,36 @@ $.fn.enterKey = function (fnc) {
 
 //TODO: instead of 'console.log' all of these should go to the popup.html, for easy copy-paste
 $(function(){
-	console.log("url should be %o", window.location.href);
+	console.log("url should be %o", window.location.pathname);
 
         $('input').enterKey(function(e){
-          console.log("I press \"enter\"");
+          console.log("I press enter");
         });
 	$('a').click(function(e) {
-		console.log("I click %o", $(e.target).text);
+          //console.log("I visit %o", $(e.target).attr("href"));
+          console.log("I click %o", $(e.target).text());
 	});
         $('select').change(function(e){
-          console.log("I select %o", $(e.target).text);
+          console.log("I select %o", $(e.target).val());
         });
-        $('input').change(function(e){
-          console.log("I fill in %o", $(e.target).text);
+        $(":text, :password").change(function(e){
+          var label = $('label[for="'+ $(e.target).attr('id')+'"]');
+          console.log("I fill in %o with %o", label.text(), $(e.target).val());
+        });
+
+        $(':checkbox').change(function(e){
+	  //if (e.val()) {
+             console.log("I check %o", $(e.target).text());
+	  //} else if {
+          //   console.log("I uncheck %o", $(e.target).text());
+	  //}
         });
 
         //TODO: things other than 'p'
         $('p').mouseup(function() {
           var text=getSelectedText();
           if (text!='') { 
-             console.log("I see %o", text);
+             console.log("I see %o", $.trim(text));
           }
         });
 
